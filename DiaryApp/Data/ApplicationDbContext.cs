763bb14a4fe147e1aaf0ad0638c9a01266e117e2
pedrohgarbim <phg.garbim@gmail.com>
2024-09-16 18:29:48 -0300

    Database connection

---
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
+
+// 1. Create a model class
+// 2. Add DbSet
+// 3. add-migration
+// 4. update-database
