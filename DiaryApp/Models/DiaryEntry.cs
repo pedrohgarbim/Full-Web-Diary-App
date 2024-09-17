3a6a27cc3e7c68b142cc16ae04e81d50ac5d9f0d
pedrohgarbim <phg.garbim@gmail.com>
2024-09-17 16:21:50 -0300

    More asp validations

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -5,7 +5,9 @@ namespace DiaryApp.Models
     public class DiaryEntry
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Enter a title")]
+        [StringLength(100, MinimumLength =3,
+            ErrorMessage ="Title must be between 3 and 100 Characters")]
         public string Title { get; set; } = string.Empty;
         [Required]
         public string Content { get; set; } = string.Empty;
